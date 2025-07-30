da4b4fbded58d64fcc92fd1d456acb42ab9d7993
Mohammed <elden4x@gmail.com>
2025-07-30 19:47:32 +0000

    Webpack is configured while Turbopack is not, which may cause problems.

--- next.config.ts
+++ next.config.ts
@@ -20,20 +20,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            async_hooks: false,
-        };
-    }
-    //
-    // It is not possible to use `dot-prompt` with webpack.
-    //
-    config.externals.push({'dot-prompt': 'commonjs dot-prompt'});
-    config.externals.push({'@opentelemetry/exporter-jaeger': 'commonjs @opentelemetry/exporter-jaeger'});
-    return config;
-  },
 };
 
 export default nextConfig;
