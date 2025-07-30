7a22104a321fd3cdff64a6d1db6f68524d221f18
mohammeioi <93697843+mohammeioi@users.noreply.github.com>
2025-07-30 23:12:06 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
-
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     unoptimized: true,
     remotePatterns: [
@@ -19,20 +12,6 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            async_hooks: false,
-        };
-    }
-    //
-    // It is not possible to use `dot-prompt` with webpack.
-    //
-    config.externals.push({'dot-prompt': 'commonjs dot-prompt'});
-    config.externals.push({'@opentelemetry/exporter-jaeger': 'commonjs @opentelemetry/exporter-jaeger'});
-    return config;
   },
 };
 
